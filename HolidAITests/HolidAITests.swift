a47bbd1949301344a8f61e9e8370383fd5434bd9
Brendo Veloso <159493555+brendoveloso@users.noreply.github.com>
2026-07-02 19:40:15 -0300

    Initial Commit

---
+++ HolidAITests/HolidAITests.swift
@@ -0,0 +1,17 @@
+//
+//  HolidAITests.swift
+//  HolidAITests
+//
+//  Created by Brendo Veloso on 02/07/26.
+//
+
+import Testing
+@testable import HolidAI
+
+struct HolidAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
